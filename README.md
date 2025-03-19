b111d74aa992b78371aaebbf762a971c0bb2c3b6
WinterSoldierUK <mattdx66@googlemail.com>
2025-03-19 16:50:50 +0000

    Added Start Timer

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 Python Pong Implementation
+
+
+TODO
+
+Power Ups
+    Speed Up Your Bat
+    Slow Down Opponent Bat
+    Increase Your Bat Length
+    Decrease Opponent Bat Length
+    Moving Power Ups?
+    His / Her Power Ups?
+    Applied Per Side?
+Acceleration Strips
--- main.py
+++ main.py
@@ -9,9 +9,10 @@ clock = pygame.time.Clock()
 
 paddle_sound = pygame.mixer.Sound('paddle hit.mp3')
 glass_sound = pygame.mixer.Sound('Glass Smash.mp3')
+air_horn_sound = pygame.mixer.Sound('air-horn.mp3')
 
 def ball_animation():
-    global ball_speed_x, ball_speed_y, player_score, opponent_score
+    global ball_speed_x, ball_speed_y, player_score, opponent_score, score_time
     ball.x += ball_speed_x
     ball.y += ball_speed_y
 
@@ -20,13 +21,12 @@ def ball_animation():
     if ball.left <= 0:
         glass_sound.play()
         player_score += 1
-        print(player_score, opponent_score)
-        ball_reset()
+        score_time = pygame.time.get_ticks()
+
     if ball.right >= screen_width:
         glass_sound.play()
         opponent_score += 1
-        print(player_score, opponent_score)
-        ball_reset()
+        score_time = pygame.time.get_ticks()
 
     if ball.colliderect(player) or ball.colliderect(opponent):
         paddle_sound.play()
@@ -50,23 +50,29 @@ def opponent_animation():
         opponent.bottom = screen_height
 
 def ball_reset():
-    global ball_speed_x, ball_speed_y
+    global ball_speed_x, ball_speed_y, score_time
+    current_time = pygame.time.get_ticks()
     ball.center = (screen_width // 2, screen_height // 2)
-    ball_speed_y *= choice((1, -1))
-    ball_speed_x *= choice((1, -1))
+
+    if current_time - score_time < 700:
+        number_three = game_font.render('3', True, light_grey)
+        screen.blit(number_three, (screen_width // 2 - 10, screen_height // 2 + 20))
+    if 700 < current_time - score_time < 1400:
+        number_two = game_font.render('2', True, light_grey)
+        screen.blit(number_two, (screen_width // 2 - 10, screen_height // 2 + 20))
+    if 1400 < current_time - score_time < 2100:
+        number_one = game_font.render('1', True, light_grey)
+        screen.blit(number_one, (screen_width // 2 - 10, screen_height // 2 + 20))
+    if current_time - score_time < 2100:
+        ball_speed_x, ball_speed_y = 0,0
+    else:
+        air_horn_sound.play()
+        ball_speed_y = 7 * choice((1, -1))
+        ball_speed_x = 7 * choice((1, -1))
+        score_time = None
 
 screen_width = 1280
 screen_height = 960
-screen = pygame.display.set_mode((screen_width, screen_height))
-pygame.display.set_caption('Pythong')
-
-ball = pygame.Rect(screen_width // 2 - 15, screen_height // 2 - 15, 30, 30)
-player = pygame.Rect(screen_width - 20, screen_height // 2 - 70, 10, 140)
-opponent = pygame.Rect(10, screen_height // 2 - 70, 10, 140)
-
-bg_colour = pygame.Color('grey12')
-light_grey = (200, 200, 200)
-
 ball_speed_x = 7
 ball_speed_y = 7
 player_speed = 0
@@ -74,6 +80,18 @@ opponent_speed = 7
 player_score = 0
 opponent_score = 0
 game_font = pygame.font.Font('freesansbold.ttf', 48)
+paddle_length = 140
+score_time = 1
+
+screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption('Pythong')
+
+ball = pygame.Rect(screen_width // 2 - 15, screen_height // 2 - 15, 30, 30)
+player = pygame.Rect(screen_width - 20, screen_height // 2 - 70, 15, paddle_length)
+opponent = pygame.Rect(10, screen_height // 2 - 70, 15, paddle_length)
+
+bg_colour = pygame.Color('grey12')
+light_grey = (200, 200, 200)
 
 while True:
     for event in pygame.event.get():
@@ -100,6 +118,9 @@ while True:
     pygame.draw.rect(screen, light_grey, opponent)
     pygame.draw.ellipse(screen, light_grey, ball)
     pygame.draw.aaline(screen, light_grey, (screen_width // 2, 0), (screen_width // 2, screen_height))
+
+    if score_time:
+        ball_reset()
 
     player_text = game_font.render(f'{player_score}', False, light_grey)
     opponent_text = game_font.render(f'{opponent_score}', False, light_grey)
